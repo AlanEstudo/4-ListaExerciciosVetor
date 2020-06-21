08a07ab92e9c92e8948367ef5b562919b46ff7f0
Alan <alanjuniorestudo@gmail.com>
2020-06-21 10:09:13 -0300

    Finalizando Exercicio 1

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,13 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+import java.util.Scanner;
+
+/*
+ * Faça um algoritmo que solicite vinte idades (um vetor de vinte posições) e ao final apresente apenas as idades nas posições pares
+ */
 /**
  *
  * @author Alan <alanjuniorestudo@gmail.com>
  */
 public class Exercicio1 {
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        /*Declarando as variaveis
+         * onde: vlIdade do tipo Vetor 
+         */
+        int vlIdade[] = new int[5];
+        System.out.println(" -------- Entradado dos dados  --------- ");
+        /*Carregando o vetor com as idades */
+        for (int i = 0; i < vlIdade.length; i++) {
+            System.out.print(" Digite a " + (i + 1) + " º idade:");
+            vlIdade[i] = teclado.nextInt();
+        }
+        /*Calculo para mostar apenas os valores PAR*/
+        System.out.println(" -------- Saida dos dados --------- ");
+        for (int i = 0; i < vlIdade.length; i++) {
+            if (vlIdade[i] % 2 == 0) {
+                System.out.println(" " + vlIdade[i]);
+            }
+        }
+    }
 }
