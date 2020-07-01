6322741b637cef246bc9888911636f624be0bb5a
Alan <alanjuniorestudo@gmail.com>
2020-07-01 06:55:26 -0300

    Iniciando exercicio 7

--- src/Exercicio7.java
+++ src/Exercicio7.java
@@ -11,27 +11,18 @@ import java.util.Scanner;
  *
  * @author Alan <alanjuniorestudo@gmail.com>
  */
-public class Exercicio6 {
+public class Exercicio7 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        float vlMedia = 0;
-        int vlValor[] = new int[18];
+        int vlPar = 0;
+        int vlValor[] = new int[8];
         
         System.out.println(" ------------ entrada dos dados -----------------");
         for (int i = 0; i < vlValor.length; i++) {
             System.out.print(" Digite o " + (i + 1) + "º valor : ");
             vlValor[i] = teclado.nextInt();
         }
-        
         /* ------------ Processamento -------------------*/
-        System.out.println(" ------------ saida dos dados -----------------");
-        int vlNegativo=0;
-        for (int i = 0; i < vlValor.length; i++){
-            if(vlValor[i] < vlNegativo){
-                vlValor[i] = 0;
-            }
-            System.out.println(+ (i + 1) + "º Valor : " + vlValor[i] );    
-        }
         
     }
 }
