631d21565d0727f12ced58cdd806ec28d3f96855
Alan <alanjuniorestudo@gmail.com>
2020-07-01 06:02:53 -0300

    Finalizando exercicio3 clear

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,17 +1,29 @@
 
 import java.util.Scanner;
 
-
-
 /**
  *
  * @author Alan <alanjuniorestudo@gmail.com>
  */
 public class Exercicio3 {
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
-        int vlValor []=new int[5];
-        
+        int vlQnt=0;
+        int vlValor[] = new int[5];
+        for (int i = 0; i < vlValor.length; i++) {
+            System.out.print(" Digite o " + (i + 1) + "º valor : ");
+            vlValor[i] = teclado.nextInt();
+        }
+
+        for (int i = 0; i < vlValor.length; i++) {
+            
+            if (vlValor[i] % 2 == 0) {
+                vlQnt ++;
+                System.out.println(" numeros pares : " + vlValor[i]);
+            }
+           
+        }
+         System.out.println(" Foram " + vlQnt + " numero pares ");
     }
 }
