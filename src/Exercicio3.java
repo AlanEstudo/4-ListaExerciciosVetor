2aeb093f8c597f3504be5112e0f4efdae8bae0c3
Alan <alanjuniorestudo@gmail.com>
2020-07-01 05:40:11 -0300

    iniciando exercicio3

---
+++ src/Exercicio3.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Alan <alanjuniorestudo@gmail.com>
+ */
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        int vlValor []=new int[5];
+        
+    }
+}
--- src/exercicio2.java
+++ src/exercicio2.java
@@ -10,15 +10,13 @@ public class exercicio2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        int vlNumero[] = new int[5];
+        int vlNumero[] = new int[10];
 
         System.out.println(" -------- Entradado dos dados  --------- ");
         for (int i = 0; i < vlNumero.length; i++) {
             System.out.print(" Digite o " + (i + 1) + " º numero:");
             vlNumero[i] = teclado.nextInt();
         }
-
-        System.out.println("--------- Saida dos dados --------------");
 
         int vlMaior = vlNumero[0];
         int vlPosicao = 0;
@@ -28,6 +26,8 @@ public class exercicio2 {
                 vlPosicao = i;
             }
         }
+        
+        System.out.println("--------- Saida dos dados --------------");
         System.out.println(" O numero maior é --: " + vlMaior);
         System.out.println(" A posição é -------: " + (vlPosicao + 1));
 
